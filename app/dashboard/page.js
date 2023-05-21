9765ac780a1d4998846710d4a085afd69123c36e
akhil <akhilvadla46@gmail.com>
2023-05-22 01:41:02 +0530

    new build

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,13 +1,16 @@
 "use client"
 import { FavoriteBorderOutlined, LocationOnOutlined, RestoreOutlined } from '@mui/icons-material';
 import { BottomNavigation, BottomNavigationAction } from '@mui/material';
+import Index from '../../src/components/EmailList/index'
 import React, { useState } from 'react'
+
 
 export default function Dashboard() {
 
     const [value,setValue]=useState("")
   return (
     <div style={{position:'absolute',bottom:0,maxWidth:'50%',margin:'auto'}}>
+        <Index />
     <BottomNavigation
     showLabels
     value={value}
